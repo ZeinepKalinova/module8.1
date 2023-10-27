95c36bfb0f368e28e865cb6325c7ddeff2302b51
ZeinepKalinova <zeinepkalinova@mail.ru>
2023-10-27 15:12:29 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class ind
+{
+    private int[] array;
+
+    public ind(int size)
+    {
+        array = new int[size];
+    }
+
+ 
+    public int this[int index]
+    {
+        get
+        {
+            return array[index];
+        }
+        set
+        {
+            array[index] = value * value;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int size = 5;
+        ind Array = new ind(size);
+
+        Array[0] = 2;
+        Array[1] = 3;
+        Array[2] = 4;
+        Array[3] = 5;
+        Array[4] = 6;
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"Элемент {i}: {Array[i]}");
+        }
+    }
+}
